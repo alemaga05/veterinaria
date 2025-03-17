407e4b4e97cee6dee8e13aefd138287c9ce5f7e0
alemaga05 <167699247+alemaga05@users.noreply.github.com>
2025-03-16 22:28:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efeito de rolagem suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
